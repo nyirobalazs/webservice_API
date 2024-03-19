3af14ce0334614fd3f6e06ff6ee06026df45cfef
Balazs Nyiro <balasz1995@gmail.com>
2024-03-19 22:53:16 +0000

    Update server.py

--- server.py
+++ server.py
@@ -6,15 +6,3 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
-class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(50))
-    date = db.Column(db.DateTime)
-
-@app.route('/events', methods=['POST'])
-def add_event():
-    data = request.get_json()
-    event = Event(type=data['type'], date=data['date'])
-    db.session.add(event)
-    db.session.commit()
-    return {'id': event.id}, 201
