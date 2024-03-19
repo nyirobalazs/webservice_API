283a596577e50c2827eb41bcaca397147f19edc8
Balazs Nyiro <balasz1995@gmail.com>
2024-03-19 22:51:15 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String(50))
+    date = db.Column(db.DateTime)
 
 @app.route('/events', methods=['POST'])
-def insert_event():
+def add_event():
     data = request.get_json()
-    for event_data in data:
-        event = Event(type=event_data['type'], date=event_data['date'])
-        db.session.add(event)
+    event = Event(type=data['type'], date=data['date'])
+    db.session.add(event)
     db.session.commit()
-    return '', 200
-
-@app.route('/control_message', methods=['GET'])
-def get_control_message():
-    # This is a placeholder. Replace this with the actual logic for retrieving the control message.
-    return jsonify({'control_message': 'sync'})
+    return {'id': event.id}, 201
