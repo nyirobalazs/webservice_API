711b0ef95ecfeaa9187321fee0c4cbca5b8ff61e
nyirobalazs <balasz1995@gmail.com>
2024-03-21 19:05:46 +0000

    v 1.1

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, redirect, url_for, request, jsonify
+import requests
+import json
 
 from .extensions import db
 from .models import Event
 
 main = Blueprint('main', __name__)
 
-@app.route('/events', methods=['POST'])
+@main.route('/events', methods=['POST'])
 def insert_event():
     data = request.get_json()
     for event_data in data:
@@ -14,18 +16,31 @@ def insert_event():
     db.session.commit()
     return '', 200
 
-@app.route('/events', methods=['GET'])
+@main.route('/events', methods=['GET'])
 def get_all_events():
     events = Event.query.all()
     return jsonify([{'type': event.type, 'date': event.date} for event in events])
 
-@app.route('/events', methods=['DELETE'])
+@main.route('/events', methods=['DELETE'])
 def clear_all_events():
     Event.query.delete()
     db.session.commit()
     return '', 200
 
-@app.route('/control_message', methods=['GET'])
-def get_control_message():
-    # This is a placeholder. Replace this with the actual logic for retrieving the control message.
-    return jsonify({'control_message': 'sync'})
+@main.route('/control_message', methods=['GET'])
+def get_control_message(self):
+    # Send a GET request to the API
+    response = requests.get(self.api_url + '/control_message', headers=self.headers, verify=True)
+
+    # Check the response
+    if response.status_code == 200:
+        # Parse the control message from the response
+        control_message = json.loads(response.text)['control_message']
+
+        # Process the control message
+        self.process_control_message(control_message)
+
+        # Post the control message to the device
+        self.post_control_message(control_message)
+    else:
+        print('Failed to get control message:', response.text)
